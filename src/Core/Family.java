951c35b9a567a153de21df940522bb6a35340979
ndtr2000 <53716352+ndtr2000@users.noreply.github.com>
2020-10-18 21:17:34 +0700

    update class

--- src/Core/Family.java
+++ src/Core/Family.java
@@ -1,5 +1,7 @@
 package Core;
 
 public class Family {
-
+	protected Person[] people;
+	
+	
 }
---
+++ src/Core/Meeting.java
@@ -0,0 +1,10 @@
+package Core;
+
+import java.util.Date;
+
+public class Meeting {
+	protected Date date;
+	protected String place;
+	protected String topic;
+	
+}
