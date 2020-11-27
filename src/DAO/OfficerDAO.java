68849fb877c697818697ba087cc48a63553120d2
Duong Minh Hoang <minhhoang161000@gmail.com>
2020-11-27 16:59:35 +0700

    OfficerDAO commit

--- src/DAO/OfficerDAO.java
+++ src/DAO/OfficerDAO.java
@@ -1,5 +1,32 @@
 package DAO;
 
+import java.io.FileInputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
+import java.util.Date;
+
+import java.util.Properties;
+
+import Core.Officer;
+
 public class OfficerDAO {
+	private  Connection myCon;
+	private SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy");
+	
+	public OfficerDAO() throws Exception {
+		Properties prop = new Properties();
+		prop.load(new FileInputStream("sql/db.properties"));
+		String user = prop.getProperty("user");
+		String password = prop.getProperty("password");
+		String dburl = prop.getProperty("dburl");
+		myCon = DriverManager.getConnection(dburl, user, password);
+	}
 
 }
