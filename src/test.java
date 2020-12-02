ff1305e23839f8bdeb07fd55069e95d4067d0210
khangt1k25 <khangt1k25>
2020-12-02 16:48:12 +0700

    update core

--- src/test.java
+++ src/test.java
@@ -8,6 +8,41 @@
  *
  * @author khangt1k25
  */
-public class test {
-    
+
+import java.util.Date;
+
+public class test{
+	protected Date startDateTerm;
+	protected Date endDateTerm;
+	
+	public Leader(String idPerson, String idFamily, String lastName, String firstName, String relationship, Date birth, String gender, String address, String email,
+			String phoneNum, String identityID, String education, String job, Date startDateTerm, Date endDateTerm) {
+		super(idPerson, idFamily, lastName, firstName, relationship, birth, gender, address, email, phoneNum, identityID, education, job);
+		this.startDateTerm = startDateTerm;
+		this.endDateTerm = endDateTerm;
+	}
+
+	public Date getStartDateTerm() {
+		return startDateTerm;
+	}
+
+	public void setStartDateTerm(Date startDateTerm) {
+		this.startDateTerm = startDateTerm;
+	}
+
+	public Date getEndDateTerm() {
+		return endDateTerm;
+	}
+
+	public void setEndDateTerm(Date endDateTerm) {
+		this.endDateTerm = endDateTerm;
+	}
+
+	@Override
+	public String toString() {
+		return "Leader [startDateTerm=" + startDateTerm + ", endDateTerm=" + endDateTerm + ", lastName=" + lastName
+				+ ", firstName=" + firstName + "]";
+	}
+	
+	
 }
