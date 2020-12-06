f79e5b1e57286a982649daab92d2b2e8ec472517
khangt1k25 <khangruni@gmail.com>
2020-12-06 15:13:31 +0700

    changed a little core

---
+++ src/Core/Khangcore.java
@@ -0,0 +1,82 @@
+package Core;
+
+import java.util.Date;
+
+public class Kh extenangcoreds Person{
+	protected String homeTown;
+	protected String university;
+	protected Date startLiving;
+	
+	public Student(String idPerson, String idFamily, String lastName, String firstName, String relationship, Date birth, String gender, String address, String email,
+			String phoneNum, String identityID, String education, String job, String homeTown, Date startLiving, String university) {
+		super(idPerson, idFamily, lastName, firstName, relationship, birth, gender, address, email, phoneNum, identityID, education, job);
+		this.homeTown = homeTown;
+		this.startLiving = startLiving;
+		this.university = university;
+	}
+
+	public String getHomeTown() {
+		return homeTown;
+	}
+
+	public void setHomeTown(String homeTown) {
+		this.homeTown = homeTown;
+	}
+
+	public String getUniversity() {
+		return university;
+	}
+
+	public void setUniversity(String university) {
+		this.university = university;
+	}
+
+	public Date getStartLiving() {
+		return startLiving;
+	}
+
+	public void setStartLiving(Date startLiving) {
+		this.startLiving = startLiving;
+	}
+}
+package Core;
+
+import java.util.Date;
+
+public class Kh extenangcoreds Person{
+	protected String homeTown;
+	protected String university;
+	protected Date startLiving;
+	
+	public Student(String idPerson, String idFamily, String lastName, String firstName, String relationship, Date birth, String gender, String address, String email,
+			String phoneNum, String identityID, String education, String job, String homeTown, Date startLiving, String university) {
+		super(idPerson, idFamily, lastName, firstName, relationship, birth, gender, address, email, phoneNum, identityID, education, job);
+		this.homeTown = homeTown;
+		this.startLiving = startLiving;
+		this.university = university;
+	}
+
+	public String getHomeTown() {
+		return homeTown;
+	}
+
+	public void setHomeTown(String homeTown) {
+		this.homeTown = homeTown;
+	}
+
+	public String getUniversity() {
+		return university;
+	}
+
+	public void setUniversity(String university) {
+		this.university = university;
+	}
+
+	public Date getStartLiving() {
+		return startLiving;
+	}
+
+	public void setStartLiving(Date startLiving) {
+		this.startLiving = startLiving;
+	}
+}
